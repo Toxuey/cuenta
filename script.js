6820f1785a68c5307c8c7a52c67c34794085bd0c
Toxuey <69595215+Toxuey@users.noreply.github.com>
2024-09-13 09:27:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Fecha objetivo (puedes cambiar esta fecha por la que desees)
-const targetDate = new Date("Sep 13, 2024 09:20:00").getTime();
+const targetDate = new Date("Sep 13, 2024 09:35:00").getTime();
 
 // Referencia al temporizador
 const countdown = setInterval(() => {
@@ -28,11 +28,13 @@ const countdown = setInterval(() => {
         // Mostrar GIF y texto animado
         document.body.innerHTML += `
             <div style="text-align: center;">
-                <h1 id="animatedText">¡La cuenta regresiva ha terminado!</h1>
+                <h1 id="animatedText">¡A beber mamahuevoooooos!</h1>
                 <div class="gif-container">
                     <img src="https://j.gifs.com/vZ47rV.gif" alt="Celebración GIF 1">
                     <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExaXMxZGhtZGpudjc2MjNwZDZ3Zm13MnppYjM3amV6cXo4cmo1MGNyMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/blSTtZehjAZ8I/giphy.gif" alt="Celebración GIF 2">
                     <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNG5mMmJhM2s3bHpsMGN0b3JkaDNudGJ1Mjd3Zzc0eWczemo0YXl5YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/wAxlCmeX1ri1y/giphy.gif">
+                    <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExd2s2NGJ1ZTdjNnMzdGRxM2Jic2hoMXBvY3Bhdm4yb3NmMGE3bXlhdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hPGloL5hneaSA/giphy.gif">
+                    <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExaXAyeWw5ZG5oNmN4bGZzdjQ2c2V4NDA2ZmJ0eGhqc3lqNDFmaTNlNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hTh9bSbUPWMWk/giphy.gif">
                 </div>
             </div>
         `;
